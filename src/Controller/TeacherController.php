b424b6efdb69711af0b884974d15cf62259dd62e
Petar Stoqnov <desireon98@gmail.com>
2022-05-08 10:26:33 +0300

    Create new controller for showing an individual teacher.

--- src/Controller/TeacherController.php
+++ src/Controller/TeacherController.php
@@ -2,15 +2,20 @@
 
 namespace App\Controller;
 
+use App\Entity\Teacher;
 use App\Filter\TeacherFilter;
 use App\Repository\CityRepository;
+use App\Repository\ReviewRepository;
 use App\Repository\SubjectRepository;
 use App\Repository\TeacherRepository;
+use App\Sorting\ReviewSorting;
+use App\Sorting\TeacherSorting;
 use Pagerfanta\Doctrine\ORM\QueryAdapter;
 use Pagerfanta\Pagerfanta;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
 class TeacherController extends AbstractController
@@ -35,11 +40,12 @@ class TeacherController extends AbstractController
         Request $request,
         TeacherFilter $teacherFilter,
         SubjectRepository $subjectRepository,
-        CityRepository $cityRepository
+        CityRepository $cityRepository,
+        TeacherSorting $teacherSorting
     ): Response
     {
         $requestOrderBy = $request->get('orderBy') ?: 'highest';
-        $orderByCriteria = $this->sortTeachers($requestOrderBy);
+        $orderByCriteria = $teacherSorting->sort($requestOrderBy);
 
         $filter = $teacherFilter->fromArray($request->query->all());
 
@@ -63,28 +69,40 @@ class TeacherController extends AbstractController
         ]);
     }
 
-    private function sortTeachers(string $filter): array
+    /**
+     * @Route("/teacher/{teacher}", name="teacher_show")
+     */
+    public function show(
+        TeacherRepository $teacherRepository,
+        ReviewSorting $reviewSorting,
+        Request $request,
+        ReviewRepository $reviewRepository
+    ): Response
     {
-        if ($filter === 'highest') {
-            return [
-                'property' => 't.rating',
-                'criteria' => 'DESC'
-            ];
-        } elseif ($filter === 'lowest') {
-            return [
-                'property' => 't.rating',
-                'criteria' => 'ASC'
-            ];
-        } elseif ($filter === 'cheapest') {
-            return [
-                'property' => 't.pricePerHour',
-                'criteria' => 'ASC'
-            ];
-        } else {
-            return [
-                'property' => 't.pricePerHour',
-                'criteria' => 'DESC'
-            ];
+        $teacher = $teacherRepository->findOneBy(['id' => $request->get('teacher'), 'status' => 'approved']);
+
+        if (is_null($teacher)) {
+            throw new NotFoundHttpException('Teacher was not found');
         }
+
+        $orderBy = $reviewSorting->sort($request->get('orderBy') ?: 'newest');
+
+        $qb = $reviewRepository->findReviewsBuilder($teacher, $orderBy);
+        $adapter = new QueryAdapter($qb);
+        $pagerfanta = new Pagerfanta($adapter);
+        $pagerfanta->setMaxPerPage(5);
+        $pagerfanta->setCurrentPage($request->get('page', 1));
+
+        $reviews = [];
+        foreach ($pagerfanta->getCurrentPageResults() as $currentPageResult) {
+            $reviews[] = $currentPageResult;
+        }
+
+        return $this->render('teacher/show.html.twig', [
+            'teacher' => $teacher,
+            'pagerfanta' => $pagerfanta,
+            'reviews' => $reviews,
+            'orderBy' => $request->get('orderBy') ?: 'newest',
+        ]);
     }
 }
