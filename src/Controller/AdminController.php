acaa6c8816b3c220c87e263ab39f2d286910e8c7
Petar Stoqnov <desireon98@gmail.com>
2022-05-29 07:32:15 +0300

    Create homepage for admin.

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ReviewRepository;
+use App\Repository\TeacherRepository;
+use Doctrine\ORM\NonUniqueResultException;
+use Doctrine\ORM\NoResultException;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdminController extends AbstractController
+{
+    /**
+     * @var ReviewRepository
+     */
+    private $reviewRepository;
+    /**
+     * @var TeacherRepository
+     */
+    private $teacherRepository;
+
+    /**
+     * @param ReviewRepository $reviewRepository
+     * @param TeacherRepository $teacherRepository
+     */
+    public function __construct(ReviewRepository $reviewRepository, TeacherRepository $teacherRepository)
+    {
+        $this->reviewRepository = $reviewRepository;
+        $this->teacherRepository = $teacherRepository;
+    }
+
+    /**
+     * @Route("/admin", name="admin")
+     * @return Response
+     * @throws NoResultException
+     * @throws NonUniqueResultException
+     */
+    public function index(): Response
+    {
+        $this->denyAccessUnlessGranted('ROLE_MODERATOR');
+
+        return $this->render('admin/index.html.twig', [
+            'reviewsCount' => $this->reviewRepository->getCountOfAll(),
+            'teachersCount' => $this->teacherRepository->getCountOfAll(),
+            'reviewsPendingCount' => $this->reviewRepository->getCountByStatus('pending'),
+            'teachersPendingCount' => $this->teacherRepository->getCountByStatus('pending'),
+        ]);
+    }
+}
